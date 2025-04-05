6c1579c2703a68f37a85ce844e604b0c93b63faf
Sakthi Sharan Mahadevan <sakthisharan@Sakthis-MacBook-Pro.local>
2025-04-05 14:27:41 -0300

    Created s3 bucket to store user resume files

---
+++ infrastructure/s3.tf
@@ -0,0 +1,22 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "resume_storage" {
+  bucket = "resume-optimizer-${random_id.suffix.hex}"
+  force_destroy = true
+
+  tags = {
+    Name        = "resume-optimizer-bucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.resume_storage.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
